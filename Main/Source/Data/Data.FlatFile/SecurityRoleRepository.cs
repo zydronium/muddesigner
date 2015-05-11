964cd41e894d9b606a70249e9f944fe6d0899587
Johnathon Sullinger <johnathon@sullinger.us>
2015-05-10 17:50:43 -0700

    Added files that were not being tracked by Visual Studio.

---
+++ Main/Source/Data/Data.FlatFile/SecurityRoleRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mud.Data.Shared;
+using Mud.Engine.Runtime.Game.Character;
+
+namespace Mud.Data.FlatFile
+{
+    public class SecurityRoleRepository : ISecurityRoleRepository
+    {
+        private IFileStorageService fileService;
+
+        public SecurityRoleRepository(IFileStorageService storageService)
+        {
+            this.fileService = storageService;
+        }
+
+        public async Task<IEnumerable<ISecurityRole>> GetSecurityRoles()
+        {
+            IEnumerable<string> roleFiles = this.fileService.GetAllFilesByExtension("role", "Security");
+            var roleTasks = new List<Task<string>>();
+            foreach (string file in roleFiles)
+            {
+                roleTasks.Add(this.fileService.LoadValueFromKeyAsync(file, "Name"));
+            }
+
+            await Task.WhenAll(roleTasks);
+            return roleTasks.Select(task => new SecurityRole(task.Result, Enumerable.Empty<ISecurityPermission>()));
+        }
+    }
+}
---
+++ Main/Source/Engine/Engine.Runtime/Game/Character/IInputCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Mud.Engine.Runtime.Game.Character
+{
+    public interface IInputCommand
+    {
+        bool IsAsyncCommand { get; }
+
+        void Execute();
+
+        Task ExecuteAsync();
+    }
+}
