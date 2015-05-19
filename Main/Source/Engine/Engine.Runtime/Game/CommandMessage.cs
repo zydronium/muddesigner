1ff2d5d743d793394de69ce448355910b259b1a6
Johnathon Sullinger <johnathon@sullinger.us>
2015-05-18 22:27:58 -0700

    Added missing CommandMessage. Visual Studio continues to default to untracking new files.

---
+++ Main/Source/Engine/Engine.Runtime/Game/CommandMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mud.Engine.Runtime.Game
+{
+    public class CommandMessage : IMessage<string>
+    {
+        public CommandMessage(string command, string content)
+        {
+            this.Command = command;
+            this.Content = content;
+        }
+
+        public string Content { get; private set; }
+
+        public string Command { get; private set; }
+
+        public object GetContent()
+        {
+            return this.Content;
+        }
+    }
+}
